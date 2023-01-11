e27d545c349512dd4872e791309f7e66431a9700
danugarri <danugarri@hotmail.com>
2023-01-11 15:01:57 +0100

    first version

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
 const readline = require("readline-sync");
 const { folderHandler } = require("./utils/foldersHandler");
-
+// rootPath
 let rootPath = "./utils/exampleFolder";
 rootPath !== undefined || rootPath !== ""
   ? readline.question(
       "\x1b[34m Type the Absolute path in your computer to folder you want to compress (As default it is ./utils/exampleFolder): \x1b[0m"
     )
   : "./utils/exampleFolder";
+//   Files to be excluded
 let excludingFiles = readline.question(
-  "\x1b[34m Are there any files to be excluded for compression: \x1b[0m"
+  "\x1b[34m Are there any files to be excluded for compression (y/n): \x1b[0m"
 );
 let excludedWord = null;
-if (excludingFiles) {
+if (excludingFiles === "y" || excludingFiles === "Y") {
   excludedWord = readline.question(
     "\x1b[34m which word include/includes the file/s you want to exclude?: \x1b[0m"
   );
 }
-
+// Destination folder
 let customDestinationFolder = "./generatedZipFile";
 
 customDestinationFolder !== undefined
   ? readline.question(
-      "\x1b[34m where do you want to store the compressed folder?( As default it is stored at ./generatedZipFile): \x1b[0m"
+      "\x1b[34m where do you want to store the compressed folder?\n( As default it is stored at ./generatedZipFile): \x1b[0m"
     )
   : "./generatedZipFile";
 console.log("#-----------------------------------------------#\n  ");
 
-// Parse folder
+// call to Parse folder
 folderHandler(rootPath, excludedWord, customDestinationFolder);
