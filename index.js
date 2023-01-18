951fd51b65da7d3e5539877a99160a0a631395ad
danugarri <danugarri@hotmail.com>
2023-01-18 22:53:43 +0100

    added input control for excluding word

--- index.js
+++ index.js
@@ -4,13 +4,11 @@ const { folderHandler } = require("./utils/foldersHandler");
 const defaultRootPath = "./utils/exampleFolder";
 let rootPath = null;
 const pathInput = readline.question(
-  "\x1b[34m Type the Absolute path in your computer to folder you want to compress (As default it is ./utils/exampleFolder): \x1b[0m"
+  "\x1b[34m Type the Absolute path in your computer to the folder you want to compress (As default it is ./utils/exampleFolder): \x1b[0m"
 );
 if (typeof pathInput === "string" && pathInput === "") {
-  console.log(typeof pathInput);
-  console.log(pathInput.length);
   rootPath = defaultRootPath;
-  console.log(rootPath);
+  console.log(`\x1b[35m selected path: ${rootPath} \n\x1b[0m`);
 }
 if (pathInput !== "") {
   rootPath = pathInput;
@@ -20,8 +18,17 @@ if (pathInput !== "") {
 let excludingFiles = readline.question(
   "\x1b[34m Are there any files to be excluded for compression (y/n): \x1b[0m"
 );
+while (excludingFiles.toLowerCase() !== "y" || excludingFiles.toLowerCase() !== "n") {
+  excludingFiles = readline.question(
+    "\x1b[34m Are there any files to be excluded for compression (y/n): \x1b[0m"
+  );
+  if (excludingFiles.toLowerCase() === "y" || excludingFiles.toLowerCase() === "n") {
+    break;
+  }
+}
+
 let excludedWord = null;
-if (excludingFiles === "y" || excludingFiles === "Y") {
+if (excludingFiles.toLowerCase() === "y") {
   excludedWord = readline.question(
     "\x1b[34m which word include/includes the file/s you want to exclude?: \x1b[0m"
   );
@@ -34,10 +41,8 @@ const destinationFolderInput = readline.question(
 );
 
 if (typeof destinationFolderInput === "string" && destinationFolderInput === "") {
-  console.log(typeof destinationFolderInput);
-  console.log(destinationFolderInput.length);
   customDestinationFolder = defaultDestinationFolder;
-  console.log(customDestinationFolder);
+  console.log(`\x1b[35m selected path: ${customDestinationFolder} \n\x1b[0m`);
 }
 if (destinationFolderInput !== "") {
   customDestinationFolder = destinationFolderInput;
